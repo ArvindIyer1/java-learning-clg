dbd965fce88b43ac5d9ae16c4c8d1fbed9d3403c
ArvindIyer1 <arvind.cooliyer@gmail.com>
2026-08-08 12:06:07 +0530

    LAB2 first lec done

--- LAB22/src/App.java
+++ LAB22/src/App.java
@@ -3,16 +3,26 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         Scanner inp = new Scanner(System.in);
-        System.out.println("Enter first number :");
-        int a = inp.nextInt();
-        System.out.println("Enter second number :");
-        int b = inp.nextInt();
-        System.out.println("Sum of two numbers is : " + (a + b));
-        System.out.println("Difference of two numbers is : " + (a - b));
-        System.out.println("Product of two numbers is : " + (a * b));
-        System.out.println("Quotient of two numbers is : " + (a / b));
-        System.out.println("Remainder of two numbers is : " + (a % b));
+        System.out.println("Enter a random number :");
+        int num = inp.nextInt();
+        System.out.println(num++);
+        System.out.println(++num);
+        System.out.println(num--);
+        System.out.println(--num);
+
+        // System.out.println("Enter first number :");
+        // int a = inp.nextInt();
+        // System.out.println("Enter second number :");
+        // int b = inp.nextInt();
+        // System.out.println("Enter third number :");
+        // int c = inp.nextInt();
+        // if(a > b && a > c){
+        //     System.out.println("A is the greatest number which is " + a);
+        // }else if(b > a && b > c){
+        //     System.out.println("B is the greatest number which is " + b);
+        // }else{
+        //     System.out.println("C is the greatest number which is " + c);
+        // }
         inp.close();
-        
     }
 }
